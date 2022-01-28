f5a7bd4e0a8a4529f3da1ce03d8c02a4f4433d09
german van de <g.vanderusten@gmail.com>
2022-01-28 11:40:30 -0300

    fix password save

--- App/Clases/UserUpdate.php
+++ App/Clases/UserUpdate.php
@@ -7,10 +7,18 @@ class UserUpdate implements UserInterface {
 
     public function execute($data) {
         $Db = new Database();
+
+        if($data->password != "")
+        {
+            $pass =  '`password` = "'.md5($data->password).'",';
+        }else
+        {
+            $pass = "";
+        }
         
         $Sql = "UPDATE `usuarios` SET 
                `nombreUsuario`= '".$data->nombreUsuario."',
-               `password`     = '".md5($data->password)."', 
+                ".$pass."
                `direccion`    = '".$data->direccion."' 
                 WHERE id      = '".$data->id."'";
 
