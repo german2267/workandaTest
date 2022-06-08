ac2f5539fb6cd428dca6faa32d705a0623f86b10
german van de R <g.vanderusten@gmail.com>
2022-06-08 18:34:29 -0300

    agregue un h2

--- login.php
+++ login.php
@@ -23,6 +23,7 @@ if(isset($_SESSION['usuarioWorkanda'])){
 <div class="login-page">
   <div class="form">
     <h1>Test Tecnico!!!</h1>
+    <h2>Hola 7mo los veo en dic</h2>
     <form class="login-form">
       <input type="text" placeholder="username" id="usuario" value="user"/>
       <input type="password" placeholder="password" id = "pass" value="1234"/>
